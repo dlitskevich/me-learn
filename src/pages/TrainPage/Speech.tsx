dba2fc7e4e05b438f98aa77b510952bccb163bed
Danila <danila.litskevich@gmail.com>
2021-08-21 21:22:16 +0300

    build

--- src/pages/TrainPage/Speech.tsx
+++ src/pages/TrainPage/Speech.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { UtterText } from '../../components/UtterText';
 import { useRecognition } from '../../hooks/useRecognition';
 import { ListenButton } from './ListenButton';
