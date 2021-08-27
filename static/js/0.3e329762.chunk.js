a3b6185191f9c413e4c70a4a401c22f789ea8cfe
Danila <danila.litskevich@gmail.com>
2021-08-27 14:20:55 +0300

    Updates

--- static/js/0.3e329762.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[0],{39:function(s){s.exports=JSON.parse('[{"id":1,"language":"english","data":[{"id":1,"title":"1000 Basic Phrases","link":"basic-phrases","progress":0},{"id":2,"title":"1000 Non=Basic Phrases","link":"basic-phrases","progress":0}]},{"id":2,"language":"deutch","data":[{"id":1,"title":"1000 Basic Phrases","link":"basic-phrases","progress":0},{"id":2,"title":"1000 Non=Basic Phrases","link":"basic-phrases","progress":0}]}]')}}]);
-//# sourceMappingURL=0.3e329762.chunk.js.map
